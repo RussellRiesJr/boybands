3229113212b5aef2f0794728a07529c41ab35ac8
Russell Ries <riesru@gmail.com>
2016-04-12 12:43:25 -0500

    complete exercise

---
+++ boybands.js
@@ -0,0 +1,40 @@
+
+
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+//The number of loops to perform (what if the array changes?)
+//[the ? = "if", the : = "else"]
+// the "if" statements below stop it from coming back undefined if one array is larger
+var loopCount = bands.length > vegetables.length ? bands.length : vegetables.length;
+
+//Keep track of which band we're on in the loop
+var currentBand = "";
+
+//Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+//Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");
+
+//Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+//Start looping
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+
+  //Add the band names into the correct <div>
+  currentBand = bands[loopTracker];
+  if (currentBand !== undefined) {
+  bandElement.innerHTML += "<div>" + currentBand + "</div>";
+  }
+
+  //Add the veggie names in the correct <div>
+  currentVeggie = vegetables[loopTracker];
+  if (currentVeggie !== undefined) {
+  veggieElement.innerHTML += "<div>" + currentVeggie + "</div>" ;
+  }
+}
+
+
